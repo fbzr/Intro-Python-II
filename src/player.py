3a538dc598c43003cfaad000add3f307b6c45de8
fbzr <fabricio.spb@gmail.com>
2020-07-08 09:44:23 -0700

    create initial player and room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room):
+        self.__set_name(name)
+
+    def __get_name(self):
+        return self._name
+    
+    def __set_name(self, name):
+        self._name = name
+
+    def __get_current_room(self):
+        return self._current_room
+    
+    def __set_current_room(self, current_room):
+        self._current_room = current_room
+
+    name = property(__get_name, __set_name)
+    current_room = property(__get_current_room, __set_current_room)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,21 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.__set_name(name)
+        self.__set_description(description)
+    
+    def __get_name(self):
+        return self._name
+    
+    def __set_name(self, name):
+        self._name = name
+
+    def __get_description(self):
+        return self._description
+    
+    def __set_description(self, description):
+        self._description = description
+
+    name = property(__get_name, __set_name)
+    description = property(__get_description, __set_description)
